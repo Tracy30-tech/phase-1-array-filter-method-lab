f82a72b17b4b1d83261035474b96d5bab17a3bc5
Tracy <tracy.atieno@student.moringaschool.com>
2022-11-27 14:19:43 +0300

    array-filter-method

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(string, name) {
+    return string.filter(
+        function (search) {
+            if (search.toUpperCase() === name.toUpperCase()) {
+                return true;
+            }
+        }
+    )
+}
+
+function fuzzyMatch(string, name) {
+    return string.filter(
+        function (search) {
+            if (search.slice(0, name.length) === name) {
+                return true;
+            }
+        }
+    )
+}
+
+function matchName(string, name) {
+    return string.filter(
+        function (search) {
+            if (search.name === name) {
+                return true;
+            }
+        }
+    )
+}
